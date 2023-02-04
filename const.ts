ff8c97e155ad9730b910dfc74291e2aa047bebcb
braedonwatkins <watkins.braedon@gmail.com>
2023-02-04 17:45:54 -0500

    publicly available constants
    may just move this with .env but no harm in exposing these

--- const.ts
+++ const.ts
@@ -1,3 +1,8 @@
-export const MAX_FRAME = 154556;
-export const MIN_FRAME = 367;
+export const MAX_FRAME = 154556; // TODO: adjust for new source
+export const MIN_FRAME = 367; // TODO: adjust for new source
+
+export const FRAME_RATE = 24;
+export const FRAME_DIR = "./fs";
+export const FRAME_NAME = "frame%s.png";
+export const VIDEO_PATH = "./spiderverse.mp4";
 
