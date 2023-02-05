de515d5a2a6a2100a294247f1808bdf52bd95191
braedonwatkins <watkins.braedon@gmail.com>
2023-02-04 19:35:50 -0500

    unecessary file name convention

--- const.ts
+++ const.ts
@@ -3,6 +3,6 @@ export const MIN_FRAME = 367; // TODO: adjust for new source
 
 export const FRAME_RATE = 24;
 export const FRAME_DIR = "./fs";
-export const FRAME_NAME = "frame%s.png";
+export const FRAME_NAME = "frame.png";
 export const VIDEO_PATH = "./spiderverse.mp4";
 
