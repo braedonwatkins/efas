0cb3f47cdacbdce83979549f0da5b4cfcfa0dcbb
braedonwatkins <watkins.braedon@gmail.com>
2023-02-04 19:34:07 -0500

    fixed async ffmpeg
    TLDR; ffmpeg didn't have async/await accounted for so it had to be wrapped in a promise.
    i don't particularly understand why but there were some github issues for fluent-ffmpeg on using it async that helped

--- ffmpeg.ts
+++ ffmpeg.ts
@@ -4,6 +4,7 @@ import ffmpeg from "fluent-ffmpeg";
 
 // TODO: simplify by just ret seconds. realized after i didn't need to construct string. those damn interview problems threw me off.
 export const numToStamp = (frameNum: number): string => {
+  frameNum.toFixed(2);
   let time = {
     h: 0,
     m: 0,
@@ -31,34 +32,38 @@ export const numToStamp = (frameNum: number): string => {
   )}:${String(time.s).padStart(2, "0")}`;
 };
 
-// TODO: FIX ASYNC BEHAVIOR WITH FFMPEG!!!
 export const getFrame = async (frameNum: number): Promise<string> => {
   //convert frame num to timestamp
   const frameStamp = numToStamp(frameNum);
   let retPath: string = "";
 
-  await ffmpeg(VIDEO_PATH)
-    .on("filenames", (filenames: string[]) => {
-      retPath = filenames[0].split("''")[0];
-      console.log(`made file: ${retPath}`);
-    })
-    .on("progress", (progress: any) => {
-      // console.log(JSON.stringify(progress));
-      console.log("Processing: " + progress.process + " KB converted");
-    })
-    .on("error", (e: Error) => {
-      console.log("Error", e);
-    })
-    .on("end", () => {
-      console.log("good job ffmpeg!");
-    })
-    .fps(FRAME_RATE)
-    .screenshots({
-      timestamps: [frameStamp],
-      filename: FRAME_NAME,
-      folder: FRAME_DIR,
-    });
+  let ret = new Promise((resolve, reject) => {
+    ffmpeg(VIDEO_PATH)
+      .on("filenames", (filenames: string[]) => {
+        retPath = filenames[0].split("''")[0];
+        console.log(`made file: ${retPath}`);
+      })
+      .on("progress", (progress) => {
+        console.log(`[ffmpeg] ${JSON.stringify(progress)}`);
+      })
+      .on("error", (err) => {
+        console.log(`[ffmpeg] error: ${err.message}`);
+        reject(err);
+      })
+      .on("end", () => {
+        resolve(`${FRAME_DIR}/${retPath}`);
+        return "[ffmpeg] finished";
+      })
+      .fps(FRAME_RATE)
+      .screenshots({
+        timestamps: [frameStamp],
+        filename: FRAME_NAME,
+        folder: FRAME_DIR,
+      });
+    // .output(`${FRAME_DIR}/${retPath}`);
+  }).then((res) => {
+    return `${res}`;
+  });
 
-  return `${FRAME_DIR}/${retPath}`;
+  return ret;
 };
-
