5752228a6b022ee114fc778c43ca4d2c936939f5
braedonwatkins <watkins.braedon@gmail.com>
2023-02-10 00:20:55 -0500

    deployment filesystem fixes

--- const.ts
+++ const.ts
@@ -4,7 +4,8 @@ export const TOTAL_FRAME = MAX_FRAME - MIN_FRAME;
 
 export const FRAME_RATE = 24;
 export const PREFIX = "~/bot";
-export const FRAME_DIR = "fs";
-export const FRAME_LIST = `${PREFIX}/${FRAME_DIR}/framelist.csv`;
+export const FRAME_DIR = `${PREFIX}/fs`;
+export const FRAME_LIST = `${FRAME_DIR}/framelist.csv`;
 export const FRAME_NAME = "frame.png";
 export const VIDEO_PATH = `${PREFIX}/spiderverse.mp4`;
+
